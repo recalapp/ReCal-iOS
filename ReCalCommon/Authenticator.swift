6f329f06f67df985a69a94ff38de600c1f6f10d0
naphatkrit <naphat.krit@gmail.com>
2014-11-08 15:41:42 -0500

    refactored DFA code

--- ReCalCommon/Authenticator.swift
+++ ReCalCommon/Authenticator.swift
@@ -18,7 +18,7 @@ public class Authenticator: AuthenticationViewControllerDelegate {
         self.authenticationUrl = NSURL(string: urlString)!
     }
     
-    private(set) public var status: AuthenticationStatus = .Unauthenticated
+    private(set) public var state: AuthenticationStatus = .Unauthenticated
     
     lazy private var authenticationNavigationController: UINavigationController = {
         let vc = UIStoryboard(name: "ReCalCommon", bundle: NSBundle(identifier: "io.recal.ReCalCommon")).instantiateViewControllerWithIdentifier(authenticationNavigationControllerStoryboardId) as UINavigationController
@@ -30,16 +30,6 @@ public class Authenticator: AuthenticationViewControllerDelegate {
     
     /// Check whether the user is authenticated. If he is, then return. Otherwise, present a view controller from rootViewController to authenticate the user. The function returns right away, so the thread is not blocked. Therefore, the caller must check the status to see if the authentication was successful
     public func authenticate() {
-        let fail: ()->Void = {
-            switch self.status {
-            case let .Authenticated(user):
-                self.status = .PreviouslyAuthenticated(user)
-            case let .PreviouslyAuthenticated(user):
-                self.status = .PreviouslyAuthenticated(user)
-            case .Unauthenticated:
-                self.status = .Unauthenticated
-            }
-        }
         let urlRequest: NSURLRequest = {
             let request = NSURLRequest(URL: self.authenticationUrl, cachePolicy: .ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
             return request
@@ -50,53 +40,69 @@ public class Authenticator: AuthenticationViewControllerDelegate {
         if let error = errorOpt {
             // connection error. Cannot do anything, so just return
             println("Error connecting. Error: \(error)")
-            fail()
+            self.advanceStateWithAuthenticationResult(.Failure)
         } else {
             if let response = responseOpt as? NSHTTPURLResponse {
                 if self.authenticationUrl.isEqual(response.URL) && response.statusCode == 200 {
                     // no redirection, and connection was successful, meaning data returned is the username
-                    let username = NSString(data: data!, encoding: NSASCIIStringEncoding)
-                    let user = User(username: username!)
-                    self.status = .Authenticated(user)
+                    let username = NSString(data: data!, encoding: NSASCIIStringEncoding) as String
+                    self.advanceStateWithAuthenticationResult(.Success(username))
                 } else {
                     // redirection occurred. Present a view controller to let the user log in
-                    fail()
+                    self.advanceStateWithAuthenticationResult(.Failure)
                     self.rootViewController.presentViewController(self.authenticationNavigationController, animated: true, completion: nil)
                 }
             } else {
-                fail()
+                self.advanceStateWithAuthenticationResult(.Failure)
+            }
+        }
+    }
+    
+    private func advanceStateWithAuthenticationResult(result: AuthenticationResult) {
+        switch self.state {
+        case .Authenticated(let user):
+            switch result {
+            case .Success(let username):
+                self.state = .Authenticated(User(username: username))
+            case .Failure:
+                self.state = .PreviouslyAuthenticated(user)
+            }
+        case .PreviouslyAuthenticated(_):
+            switch result {
+            case .Success(let username):
+                self.state = .Authenticated(User(username: username))
+            case .Failure:
+                break
+            }
+        case .Unauthenticated:
+            switch result {
+            case .Success(let username):
+                self.state = .Authenticated(User(username: username))
+            case .Failure:
+                break
             }
         }
     }
     
     // MARK: - Authentication View Controller Delegate
     func authenticationDidCancel(authenticationViewController: AuthenticationViewController) {
-        switch self.status {
-        case let .Authenticated(user):
-            self.status = .PreviouslyAuthenticated(user)
-        case let .PreviouslyAuthenticated(user):
-            self.status = .PreviouslyAuthenticated(user)
-        case .Unauthenticated:
-            self.status = .Unauthenticated
-        }
+        self.advanceStateWithAuthenticationResult(.Failure)
         self.rootViewController.dismissViewControllerAnimated(true, completion: nil)
     }
     func authentication(authenticationViewController: AuthenticationViewController, didAuthenticateWithUsername username: String) {
-        let user = User(username: username)
-        self.status = .Authenticated(user)
+        self.advanceStateWithAuthenticationResult(.Success(username))
         self.rootViewController.dismissViewControllerAnimated(true, completion: nil)
     }
     func authenticationDidFail(authenticationViewController: AuthenticationViewController) {
-        switch self.status {
-        case let .Authenticated(user):
-            self.status = .PreviouslyAuthenticated(user)
-        case let .PreviouslyAuthenticated(user):
-            self.status = .PreviouslyAuthenticated(user)
-        case .Unauthenticated:
-            self.status = .Unauthenticated
-        }
+        self.advanceStateWithAuthenticationResult(.Failure)
         self.rootViewController.dismissViewControllerAnimated(true, completion: nil)
     }
+}
+
+/// used as input to DFA
+private enum AuthenticationResult {
+    case Success(String)
+    case Failure
 }
 
 public enum AuthenticationStatus {
